4ff230a7dba67b2d1996ec26ddb12d1b1cc68ead
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-16 23:45:19 +0600

    add visited country styling and update Country component to reflect visit status

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -9,7 +9,7 @@ const Country = ({ country }) => {
         setVisited(!visited);
     }
         return (
-        <div className='country'>
+        <div className={`'country' ${visited && 'visited-country'}`}>
             <img className='card-container' src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h4>Name: {country.name.common}</h4>
             <p>Population: {country.population.population}</p>
