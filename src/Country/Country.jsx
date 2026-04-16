cdd4283d3819400cd6fb3439d463b3a2120e25fa
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-16 22:29:58 +0600

    add image display for country flags in Country component

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -4,6 +4,7 @@ const Country = ({ country }) => {
     // console.log(country);
     return (
         <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h4>Name: {country.name.common}</h4>
             <p>Population: {country.population.population}</p>
         </div>
