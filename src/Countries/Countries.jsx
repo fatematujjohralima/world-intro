cfcfadedd18a33a9de19ac8d53e86f6a0216ed11
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-16 22:05:20 +0600

    load countries data from API with Suspense and use() hook

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -2,11 +2,10 @@ import React, { use } from 'react';
 
 const Countries = ({ fetchCountries }) => {
     const countries = use(fetchCountries);
-    const allCountries = countries.countries;
-    console.log(allCountries);
+    console.log(countries);
     return (
         <div>
-            <h3>There are {allCountries.length} countries in the world.</h3>
+            
         </div>
     );
 };
