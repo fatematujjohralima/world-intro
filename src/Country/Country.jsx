5ca5184e46b84440a017c3eb6473f5cccc46d4e8
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-16 23:16:55 +0600

    add visited state and button to track country visit status in Country component

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({ country }) => {
     // console.log(country);
-    return (
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+        return (
         <div className='country'>
             <img className='card-container' src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h4>Name: {country.name.common}</h4>
             <p>Population: {country.population.population}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
+
+
 
 export default Country;
